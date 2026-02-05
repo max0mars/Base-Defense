984b9644b6e340ea3665d5c9346a7c701cd2371a
max0mars <marslandmax@gmail.com>
2026-02-04 23:39:56 -0500

    stat change

--- Enemies/Speeder.lua
+++ Enemies/Speeder.lua
@@ -3,7 +3,7 @@ local speeder = setmetatable({}, {__index = Enemy})
 speeder.__index = speeder
 
 local default = {
-    speed = 70, -- Set speed for speeder
+    speed = 120, -- Set speed for speeder
     hp = 25, -- Set health for speeder
     maxHp = 25,
     color = {0, 1, 0, 1}, -- Default color for basic enemies
