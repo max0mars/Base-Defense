3051b24a68d923e8da2eeae27876d257230fc9fc
max0mars <maxmarsyyz@icloud.com>
2026-05-24 14:01:52 -0400

    fix: image scaling

--- main.lua
+++ main.lua
@@ -29,6 +29,7 @@ scene_manager.current = scene_manager.scenes.menu -- Set the initial scene to me
 local AudioManager = require("Audio.AudioManager")
 
 function love.load()
+    love.graphics.setDefaultFilter("nearest", "nearest")
     love.window.setTitle("Base Defense")
     scalify:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, 800, 600, { resizable = true, vsync = true, highdpi = true})
     scalify:setBorderColor(0.1, 0.1, 0.1)
