73cd51d784c5397268c301bddb7196eebd885d41
max0mars <maxmarsyyz@icloud.com>
2026-06-19 10:54:04 -0400

    phase 1

--- Game/Spawning/BattleDirector.lua
+++ Game/Spawning/BattleDirector.lua
@@ -3,14 +3,35 @@ local WaveDirector = require("Game.Spawning.WaveDirector")
 local BattleDirector = {}
 BattleDirector.__index = BattleDirector
 
+BattleDirector.ExampleProfiles = {
+    Standard = {
+        isSpecialEvent = false,
+        allowedTiers = {1, 2, 3},
+        maxDistinctEnemyTypes = 5,
+        allowedTypes = {"melee", "ranged", "magic", "goblin"}
+    },
+    BossAmbush = {
+        isSpecialEvent = true,
+        allowedTiers = {3, 4, 5},
+        maxDistinctEnemyTypes = 2,
+        allowedTypes = {"boss", "elite", "melee"}
+    }
+}
+
 function BattleDirector:new(game)
     local obj = setmetatable({}, self)
     obj.game = game
     return obj
 end
 
-function BattleDirector:generateBattle(globalDifficulty)
+function BattleDirector:generateBattle(globalDifficulty, profile)
     local wd = WaveDirector:new(self.game)
+    
+    if profile then
+        print("maxDistinctEnemyTypes from profile: " .. tostring(profile.maxDistinctEnemyTypes))
+    else
+        print("maxDistinctEnemyTypes from profile: nil")
+    end
     
     local upcomingWaves = {}
     local upcomingSummaries = {}
